f5674a6db72779e5d2fce2c039c752ded28ec5dd
KennyDied <thekennydied@mail.ru>
2021-02-12 15:59:52 +0300

    add part transofm

--- BWproj/script.js
+++ BWproj/script.js
@@ -1,17 +1,59 @@
-
-something.onmouseover = function() {
-    this.style.backgroundColor = 'red';
-};
-something.onmouseout = function() {
-    this.style.backgroundColor = '';
-};
+const screenW = window.screen.width;
+const screenH = window.screen.height;
 
 
 function scaleLeftPart(){
-    var widthOfPart = document.getElementById();
+    
+    var leftElement = document.getElementById("left__wrapper");
+    var rightElement = document.getElementById("right__wrapper");
+
+    var rightText = document.getElementById("r__white");
+
+
+    leftElement.style.width = 75 + "%";
+    leftElement.style.background = "black";
+
+
+    rightElement.style.width = 25 + "%";
+    rightText.style.display = "none";    
 
 }
 
+
+
 function scaleRightPart(){
-    alert("нет ты пидор");
+    var leftElement = document.getElementById("left__wrapper");
+    var rightElement = document.getElementById("right__wrapper");
+
+    var leftText = document.getElementById("s__black");
+
+    
+    leftElement.style.width = 25 + "%";
+    leftText.style.display = "none";    
+
+
+    rightElement.style.width = 75 + "%";
+    rightElement.style.background = "white";
+
+
+
+}
+
+function returnLeftPart(){
+    var leftElement = document.getElementById("left__wrapper");
+    var rightElement = document.getElementById("right__wrapper");
+    var lefttText = document.getElementById("s__black");
+    var size = leftElement.style.width;
+
+    if (leftElement.style.width = "25%"){
+        leftElement.style.width = 50 + "%";
+        leftElement.style.background = "#202020";
+
+
+        rightElement.style.width = 50 + "%";
+        lefttText.style.display = "inline";  
+    } else {
+        scaleLeftPart();
+    }
+    
 }
