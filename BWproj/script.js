986d4118a7eb927417587ad1a061f7b0b17d3f90
KennyDied <thekennydied@mail.ru>
2021-02-12 14:32:42 +0300

    add script file

---
+++ BWproj/script.js
@@ -0,0 +1,17 @@
+
+something.onmouseover = function() {
+    this.style.backgroundColor = 'red';
+};
+something.onmouseout = function() {
+    this.style.backgroundColor = '';
+};
+
+
+function scaleLeftPart(){
+    var widthOfPart = document.getElementById();
+
+}
+
+function scaleRightPart(){
+    alert("нет ты пидор");
+}
