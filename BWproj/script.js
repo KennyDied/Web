1359a131ba7afe12675cbc140e09676267e1342c
KennyDied <thekennydied@mail.ru>
2021-02-28 17:29:16 +0300

    add slider

--- BWproj/script.js
+++ BWproj/script.js
@@ -128,65 +128,77 @@ function returnAll(){
 
 
 //Slider//
-$(function() {      
-	var slider = $('.slider'),
-	  sliderContent = slider.html(),                      // Содержимое слайдера
-	  slideWidth = $('.slider-box').outerWidth(),         // Ширина слайдера
-	  slideCount = $('.slider img').length,               // Количество слайдов
-	  prev = $('.slider-box .prev'),                      // Кнопка "назад"
-	  next = $('.slider-box .next'),                      // Кнопка "вперед"
-	  sliderInterval = 3300,                              // Интервал смены слайдов
-	  animateTime = 1000,                                 // Время смены слайдов
-	  course = 1,                                         // Направление движения слайдера (1 или -1)
-	  margin = - slideWidth;                              // Первоначальное смещение слайдов
-   
-	$('.slider img:last').clone().prependTo('.slider');   // Копия последнего слайда помещается в начало.
-	$('.slider img').eq(1).clone().appendTo('.slider');   // Копия первого слайда помещается в конец.  
-	$('.slider').css('margin-left', -slideWidth);         // Контейнер .slider сдвигается влево на ширину одного слайда.
-   
-	function nextSlide(){                                 // Запускается функция animation(), выполняющая смену слайдов.
-	  interval = window.setInterval(animate, sliderInterval);
-	}
-   
-	function animate(){
-	  if (margin==-slideCount*slideWidth-slideWidth){     // Если слайдер дошел до конца
-		slider.css({'marginLeft':-slideWidth});             // то блок .slider возвращается в начальное положение
-		margin=-slideWidth*2;
-	  }else if(margin==0 && course==-1){                  // Если слайдер находится в начале и нажата кнопка "назад"
-		slider.css({'marginLeft':-slideWidth*slideCount});  // то блок .slider перемещается в конечное положение
-		margin=-slideWidth*slideCount+slideWidth;
-	  }else{                                              // Если условия выше не сработали,
-	  margin = margin - slideWidth*(course);              // значение margin устанавливается для показа следующего слайда
-	  }
-	  slider.animate({'marginLeft':margin},animateTime);  // Блок .slider смещается влево на 1 слайд.
-	}
-   
-	function sliderStop(){                                // Функция преостанавливающая работу слайдера      
-	  window.clearInterval(interval);
-	}
-   
-	prev.click(function() {                               // Нажата кнопка "назад"
-	  if (slider.is(':animated')) { return false; }       // Если не происходит анимация
-	  var course2 = course;                               // Временная переменная для хранения значения course
-	  course = -1;                                        // Устанавливается направление слайдера справа налево
-	  animate();                                          // Вызов функции animate()
-	  course = course2 ;                                  // Переменная course принимает первоначальное значение
-	});
-	next.click(function() {                               // Нажата кнопка "назад"
-	  if (slider.is(':animated')) { return false; }       // Если не происходит анимация
-	  var course2 = course;                               // Временная переменная для хранения значения course
-	  course = 1;                                         // Устанавливается направление слайдера справа налево
-	  animate();                                          // Вызов функции animate()
-	  course = course2 ;                                  // Переменная course принимает первоначальное значение
-	});
-   
-	slider.add(next).add(prev).hover(function() {         // Если курсор мыши в пределах слайдера
-	  sliderStop();                                       // Вызывается функция sliderStop() для приостановки работы слайдера
-	}, nextSlide);                                        // Когда курсор уходит со слайдера, анимация возобновляется.
-   
-	nextSlide();                                          // Вызов функции nextSlide()
-  });
+var slideNow = 1;
+var slideCount = $('#slidewrapper').children().length;
+var slideInterval = 3000;
+var navBtnId = 0;
+var translateWidth = 0;
+var switchInterval = setInterval(nextSlide, slideInterval);
 
-  //http://coderpro.ru/jquery-prostoj-slajder-s-knopkami-vpered-i-nazad.html -  БЛЯТЬ ПРОСТО ЗАРАБОТАЙ НАХУЙ
+    $('#viewport').hover(function() {
+        clearInterval(switchInterval);
+    }, function() {
+        switchInterval = setInterval(nextSlide, slideInterval);
+    });
+
+    $('#next-btn').click(function() {
+        nextSlide();
+    });
+
+    $('#prev-btn').click(function() {
+        prevSlide();
+    });
+
+    $('.slide-nav-btn').click(function() {
+        navBtnId = $(this).index();
+
+        if (navBtnId + 1 != slideNow) {
+            translateWidth = -$('#viewport').width() * (navBtnId);
+            $('#slidewrapper').css({
+                'transform': 'translate(' + translateWidth + 'px, 0)',
+                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            });
+            slideNow = navBtnId + 1;
+        }
+    });
+
+
+function nextSlide() {
+    if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+        $('#slidewrapper').css('transform', 'translate(0, 0)');
+        slideNow = 1;
+    } else {
+        translateWidth = -$('#viewport').width() * (slideNow);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow++;
+    }
+}
+
+function prevSlide() {
+    if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+        translateWidth = -$('#viewport').width() * (slideCount - 1);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow = slideCount;
+    } else {
+        translateWidth = -$('#viewport').width() * (slideNow - 2);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow--;
+    }
+}
+
+//https://habr.com/ru/post/319394/ - слайдер брал тут
 
 });
